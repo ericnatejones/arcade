2fa17b65626fcbf12c2717101fe9d6504ba377ce
Eric Jones <ericnatejones@gmail.com>
2020-04-22 16:12:30 -0600

    master mind work

--- src/masterMind/Guesses.js
+++ src/masterMind/Guesses.js
@@ -18,8 +18,15 @@ export default function Guesses(props) {
         })
     }
 
+    const lockedInGuesses = props.guesses.map(guess => {
+        return <div>{guess.map(color => <div 
+            style={{backgroundColor: color}} className="option set">
+        </div>)}</div>
+    })
+
     return (
         <div>
+            {lockedInGuesses}
             <Guess onChange={handleChange} guess="0" color={guesses[0]}/>
             <Guess onChange={handleChange} guess="1" color={guesses[1]}/>
             <Guess onChange={handleChange} guess="2" color={guesses[2]}/>
--- src/masterMind/index.js
+++ src/masterMind/index.js
@@ -1,25 +1,44 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {createSolution} from "../assets/masterMind"
 import Guesses from './Guesses'
 import Clues from './Clues'
-import ColorSelector from './ColorSelector'
 
 export default function MasterMind() {
     const [solution, setSolution] = useState(createSolution())
     const [guesses, setGuesses] = useState([])
     const [clues, setClues] = useState([])
 
+    useEffect(()=>{
+        setSolution(createSolution)
+    }, [])
+
     const handleSubmit = (guess) => {
+        console.log(guess)
+        const newClues = []
+       
+        for(let i = 0; i < guess.length; i++){
+           
+            if(guess[i] === solution[i]){
+                newClues.push("black")
+                
+            } else if(solution.includes(guess[i]) && guess[i] !== solution[i]){
+                newClues.push("white")
+            }
+           
+            console.log(newClues)
+
+        }
+        clues.sort()
+        setClues(prevClues => [...prevClues, newClues])
         setGuesses(prevGuesses => [...prevGuesses, guess])
     }
 
     return (
         <>
         <div className="master-mind-board">
-            <Guesses submitGuess={handleSubmit}/>
+            <Guesses submitGuess={handleSubmit} guesses={guesses}/>
             <Clues/>
         </div>
-        <ColorSelector/>
         </>
     )
 }
