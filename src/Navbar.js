c7c253de9495a7fa949faf7373e73157431d64bf
Bob Ziroll <elderziroll@gmail.com>
2020-08-04 15:06:44 -0600

    initial commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
-
+// Sweet navbar. Really "pops".
 export default function Navbar() {
-    return (
-        <div className="navbar">
-            <Link to="/">home</Link>
-            <Link to="/snake">snake</Link>
-            <Link to="/frogger">frogger</Link>
-            <Link to="/mine-sweeper">mine sweeper</Link>
-            <Link to="/master-mind">master mind</Link>
-        </div>
-    )
+  return (
+    <div className="navbar">
+      <Link to="/">home</Link>
+      <Link to="/snake">snake</Link>
+      <Link to="/frogger">frogger</Link>
+      <Link to="/mine-sweeper">mine sweeper</Link>
+      <Link to="/master-mind">master mind</Link>
+    </div>
+  );
 }
