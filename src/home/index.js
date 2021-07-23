3680a6c1290a533b0fb4960a48b189c8de83c6a4
Eric Jones <ericnatejones@gmail.com>
2021-07-23 09:24:53 -0700

    home page

--- src/home/index.js
+++ src/home/index.js
@@ -4,10 +4,12 @@ import React from 'react'
 export default function index() {
     return (
         <div>
+            yo my dawgs, welcome to the arcade. Frogger is Nate's. Master mind is a work in progress. Mine Sweeper works, but has some kinks that are difficult to figure out. Snake is sick af. 
             {/* <Tile game="" notes="" image={}/>
             <Tile game="" notes="" image={}/>
             <Tile game="" notes="" image={}/>
             <Tile game="" notes="" image={}/> */}
+            <h6>shrig brown church</h6>
         </div>
     )
 }
