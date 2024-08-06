6a49197c957864ad78534a9be52ad8f78382c9b9
Eric Jones <ericnatejones@gmail.com>
2024-08-06 11:27:28 -0700

    Update index.js to make it a little weirder, but authentic

--- src/home/index.js
+++ src/home/index.js
@@ -4,12 +4,22 @@ import React from 'react'
 export default function index() {
     return (
         <div>
+            <p>
             Welcome to the arcade. More styling to come in the future, but I've pressing matters. Minesweeper needs a restart before you can play, but works great! It was tough to build. snake is great! and way easier to build in vanilla js. But this is the React Arcade! React games only! 
             Instructions are needed for them all, but consider it a hard game mechanic that you have to figure them out, eh? 
-            {/* <Tile game="" notes="" image={}/>
+            </p>
+            <p>
+            If you're part of Code Pals, and have discovered this, a great game for you to work on to display as your own is:
+            https://react-mancala.netlify.app/
+            </p>
+            <p>
+                this page itself needs work, and the comments below show my intentions. Students of mine can use it for practice, and potential employers of mine can use it to see how responsive I have to feedback and direction. 
+    </p>        
+    {/* <Tile game="" notes="" image={}/>
             <Tile game="" notes="" image={}/>
             <Tile game="" notes="" image={}/>
             <Tile game="" notes="" image={}/> */}
+            
             
         </div>
     )
