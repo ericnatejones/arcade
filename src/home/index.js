a8919d4466f6ed6bf4a788e8326e817380d1196e
Eric Jones <ericnatejones@gmail.com>
2024-08-16 08:34:25 -0600

    Update index.js

--- src/home/index.js
+++ src/home/index.js
@@ -5,7 +5,7 @@ export default function index() {
     return (
         <div>
             <p>
-            Welcome to the arcade. More styling to come in the future, but I've pressing matters. Minesweeper needs a restart before you can play, but works great! It was tough to build. snake is great! and way easier to build in vanilla js. But this is the React Arcade! React games only! 
+            Welcome to the arcade. More editing and styling to come in the future, but I've pressing matters. Minesweeper needs a restart before you can play, but works great! It was tough to build. snake is great! and way easier to build in vanilla js. But this is the React Arcade! React games only! 
             Instructions are needed for them all, but consider it a hard game mechanic that you have to figure them out, eh? 
             </p>
             <p>
